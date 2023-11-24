cb30f6ae178540fe090bb48fd09a743013fd2f49
rodrigorvsn <rodrigovictor81@gmail.com>
2023-11-24 19:48:45 -0300

    feat: add ping and 'carente' commands

---
+++ src/main.rs
@@ -0,0 +1,55 @@
+use std::env;
+
+use dotenv::dotenv;
+use serenity::async_trait;
+use serenity::model::channel::Message;
+use serenity::model::gateway::Ready;
+use serenity::model::id::UserId;
+use serenity::prelude::*;
+
+struct Handler;
+
+#[async_trait]
+impl EventHandler for Handler {
+    async fn message(&self, ctx: Context, msg: Message) {
+        if msg.content == "!ping" {
+            if let Err(why) = msg.channel_id.say(&ctx.http, "Pong!").await {
+                println!("Error sending message: {why:?}");
+            }
+        }
+
+        if msg.content == "carente" {
+            let user_id = env::var("USER_ID").expect("Expected a user id in the environment");
+            let target_user_id = UserId(user_id.parse::<u64>().unwrap());
+            if let Err(why) = msg
+                .channel_id
+                .say(&ctx.http, format!("Bom dia <@{}>!", target_user_id.0))
+                .await
+            {
+                println!("Error sending message: {why:?}");
+            }
+        }
+    }
+
+    async fn ready(&self, _: Context, ready: Ready) {
+        println!("{} is connected!", ready.user.name);
+    }
+}
+
+#[tokio::main]
+async fn main() {
+    dotenv().ok();
+    let token = env::var("DISCORD_TOKEN").expect("Expected a token in the environment");
+    let intents = GatewayIntents::GUILD_MESSAGES
+        | GatewayIntents::DIRECT_MESSAGES
+        | GatewayIntents::MESSAGE_CONTENT;
+
+    let mut client = Client::builder(&token, intents)
+        .event_handler(Handler)
+        .await
+        .expect("Err creating client");
+
+    if let Err(why) = client.start().await {
+        println!("Client error: {why:?}");
+    }
+}
